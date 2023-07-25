a77b05d4420b32dbd77ca93be2999a70152ccea4
hsm <hhpp000222@gamil.com>
2023-07-25 21:40:50 +0900

    first commit

---
+++ src/main/java/kr/meal/polyMealServer/PolyMealServerApplication.java
@@ -0,0 +1,13 @@
+package kr.meal.polyMealServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyMealServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyMealServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/meal/polyMealServer/PolyMealServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.meal.polyMealServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyMealServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
